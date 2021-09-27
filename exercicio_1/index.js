0027bf28df004a9721b01c328b5773bb98d77f48
TairineEllen <tairine.ellen@gmail.com>
2021-09-27 15:42:56 -0300

    create a menu to see the available seats and to book a seat

--- exercicio_1/index.js
+++ exercicio_1/index.js
@@ -1,10 +1,11 @@
 const Theater = require('./theater');
+const { BookedSeatError } = require('./utils/errors');
 
 
 const readline = require('readline');
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
 const theater = new Theater();
@@ -25,7 +26,36 @@ function openMainMenu() {
       default:
         console.log('\nOpção inválida!\n');
         openMainMenu();
-    }    
+    }
+  })
+}
+
+function openBookingMenu() {
+  console.log('\n========== Lugares Disponíveis\n')
+  console.log(theater.showAvailableSeats());
+  rl.question('\n========== Realizar reserva\n 1. Reservar poltrona\n 2. Voltar \n\n Escolha uma opção: ', opt => {
+    switch (opt) {
+      case '1':
+        rl.question('\nEscolha a poltrona (ex: A1): ', opt => {
+          try {
+            theater.bookSeat(opt);
+            console.log(`\nPoltrona ${opt.toUpperCase()} reservada com sucesso!`);
+            openMainMenu();
+          } catch (error) {
+            error instanceof BookedSeatError
+              ? console.log(error.message)
+              : console.log('\nEntrada inválida. Tente novamente.\n');
+            openBookingMenu();
+          }
+        })
+        break;
+      case '2':
+        openMainMenu();
+        break;
+      default:
+        console.log('\nEntrada inválida. Tente novamente.\n');
+        openBookingMenu();
+    }
   })
 }
 
