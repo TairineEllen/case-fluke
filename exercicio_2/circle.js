c19728e3ddc5f8f2535ee0e44a40a23d66590bde
TairineEllen <tairine.ellen@gmail.com>
2021-09-30 10:32:50 -0300

    add a function to get radius value via terminal

--- exercicio_2/circle.js
+++ exercicio_2/circle.js
@@ -1,10 +1,13 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 function drawCircle(radius) {
   let diameter;
-  if (radius % 2 === 0) {
-    diameter = 2 * radius;
-  } else {
-    diameter = (2 * radius) - 1;
-  }
+  (radius % 2 === 0) ? diameter = 2 * radius : diameter = (2 * radius) - 1;
+
   const circle = Array(diameter).fill(' ').map(() => Array(diameter).fill(' '));
   let count = 0;
   let draw = radius;
@@ -19,17 +22,9 @@ function drawCircle(radius) {
       }
     }
     if (count < radius) {
-      if (draw + 2 <= diameter) {
-        draw += 2;
-      } else {
-        draw = diameter;
-      }
+      (draw + 2 <= diameter) ? draw += 2 : draw = diameter;
     } else {
-      if (draw - 2 >= radius) {
-        draw -= 2;
-      } else {
-        draw = radius;
-      }
+      (draw - 2 >= radius) ? draw -= 2 : draw = radius;
     }
     if (draw === diameter) {
       count++;
@@ -46,4 +41,16 @@ function drawCircle(radius) {
   })
 }
 
-drawCircle(5)
+(function question() {
+  rl.question('Digite o raio: ', rad => {
+    let radius = Number(rad);
+    if (radius < 1 || radius > 100) {
+      console.log('Por favor, digite um número de 1 a 100!');
+      question();
+    }
+    else {
+      drawCircle(radius);
+      rl.close();
+    }
+  })
+})();
