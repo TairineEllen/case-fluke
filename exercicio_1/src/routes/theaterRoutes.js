34d3cab8963345fe7b64aca015eab0e169271519
TairineEllen <tairine.ellen@gmail.com>
2021-09-30 21:02:19 -0300

    update POST route

--- exercicio_1/src/routes/theaterRoutes.js
+++ exercicio_1/src/routes/theaterRoutes.js
@@ -8,7 +8,7 @@ router.get('/livres', controller.showAvailable);
 router.get('/mapa', controller.showMap);
 router.get('/total', controller.getTotalSeats);
 router.get('/arrecadacao', controller.calculateAmount)
-router.post('/', controller.newBooking);
+router.post('/reservar', controller.newBooking);
 router.put('/confirmar', controller.confirmBookedSeat);
 router.put('/retirar', controller.removeBookedSeat);
 
