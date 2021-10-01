7ef1c966f6dc8594471a8eea151396c0b270e275
TairineEllen <tairine.ellen@gmail.com>
2021-09-30 21:59:14 -0300

    add error handling

--- exercicio_1/src/controllers/theaterController.js
+++ exercicio_1/src/controllers/theaterController.js
@@ -16,42 +16,70 @@ class TheaterController {
 
   newBooking(req, res) {
     const { seat } = req.body;
-    this._theater.bookSeat(seat);
-    res.status(200).send('Reserva feita com sucesso');
+    try {
+      this._theater.bookSeat(seat);
+      res.status(200).send('Reserva feita com sucesso');
+    } catch (error) {
+      res.status(424).send(error.message);
+    }
   }
   showAvailable(req, res) {
-    const teste = this._theater.showAvailableSeats();
-    res.status(200).send(teste);
+    try {
+      const seats = this._theater.showAvailableSeats();
+      res.status(200).send(seats);
+    } catch (error) {
+      res.status(424).send(error.message);
+    }
   }
   showMap(req, res) {
-    const map = this._theater.showSeatMap();
-    res.status(200).send(map);
+    try {
+      const map = this._theater.showSeatMap();
+      res.status(200).send(map);
+    } catch (error) {
+      res.status(424).send(error.message);
+    }
   }
   confirmBookedSeat(req, res) {
     const { seat } = req.body;
-    this._theater.confirmBooking(seat);
-    res.status(200).send('Reserva confirmada com sucesso')
+    try {
+      this._theater.confirmBooking(seat);
+      res.status(200).send('Reserva confirmada com sucesso')
+    } catch (error) {
+      res.status(424).send(error.message);
+    }
   }
   removeBookedSeat(req, res) {
     const { seat } = req.body;
-    this._theater.removeBooking(seat)
-    res.status(200).send('Reserva removida com sucesso')
+    try {
+      this._theater.removeBooking(seat);
+      res.status(200).send('Reserva removida com sucesso');
+    } catch (error) {
+      res.status(424).send(error.message);
+    }
   }
   getTotalSeats(req, res) {
-    const available = this._theater.showTotal('L');
-    const booked = this._theater.showTotal('R');
-    const confirmed = this._theater.showTotal('C');
-    res.status(200).send({
-      livres: available,
-      reservados: booked,
-      confirmados: confirmed
-    })
+    try {
+      const available = this._theater.showTotal('L');
+      const booked = this._theater.showTotal('R');
+      const confirmed = this._theater.showTotal('C');
+      res.status(200).send({
+        livres: available,
+        reservados: booked,
+        confirmados: confirmed
+      })
+    } catch (error) {
+      res.status(424).send(error.message);
+    }
   }
   calculateAmount(req, res) {
-    const total = this._theater.calculateCollectedAmount();
-    res.status(200).send({
-      totalArrecadado: total
-    });
+    try {
+      const total = this._theater.calculateCollectedAmount();
+      res.status(200).send({
+        totalArrecadado: total
+      });
+    } catch (error) {
+      res.status(424).send(error.message);
+    }
   }
 }
 
